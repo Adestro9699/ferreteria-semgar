9d95e99d89b609bf5d2665c60eb1dc8fb725d1f6
SemGar96 <argossg96@gmail.com>
2025-05-14 15:31:01 -0500

    DEV y PROD (Corregidos v0.1)

--- src/main/java/com/semgarcorp/ferreteriaSemGar/FerreteriaSemGarApplication.java
+++ src/main/java/com/semgarcorp/ferreteriaSemGar/FerreteriaSemGarApplication.java
@@ -2,18 +2,11 @@ package com.semgarcorp.ferreteriaSemGar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class FerreteriaSemGarApplication extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(FerreteriaSemGarApplication.class);
-	}
+public class FerreteriaSemGarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FerreteriaSemGarApplication.class, args);
